bdfa6ddc37adf3bdc7ef2b4f43fea7e27be7e199
Abdumajidovv <150424164+Abdumajidovv@users.noreply.github.com>
2024-04-23 06:54:26 +1030

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+      <div className="container">
+        <div className="wrapper">
+          <img src="https://t4.ftcdn.net/jpg/03/08/82/39/360_F_308823955_XTMT8TNKmOYnPEwmEmfnskgNqQv3hQE5.jpg" alt="" />
+
+          <h1>  Абдумажидов Илхом  Алишер УГЛИ</h1>
+          <br />
+          <b>FronTend разработчик </b>
+          <br />
+          <br />
+          <br />
+          <p className='ppp'>ПЕРСОНАЛЬНЫЕ ДАННЫЕ</p>
+          <br />
+          <p>
+            Сергели район, Дарйо бойы Махалля, 6 берк коча, 2- Уй <br />
+            Abdumajidovvilhom@gmail.com , +998 33 954 40 48 <br />
+            Дата рождения: 5 ийул 2004 г. <br />
+            Место рождения: Ташкент
+          </p>
+
+          <br />
+          <br />
+          <p className='ppp'> ОБРАЗОВАНИЕ</p>
+          <br />
+
+          <p>
+            53-школа, Ташкент   сент. 2011 - май 2022 <br />
+            Закончил школу в 2022 году. В 2023 начал учить IT (Frontend).
+          </p>
+          <br />
+          <br />
+          <p className='ppp'>Место учебы</p>
+          <br />
+          <p>IT ACADEMY</p>
+
+          <br />
+          <br />
+          <p className='ppp' >НАВЫКИ</p>
+          <br />
+          <p>
+            HTML, CSS, Bootstrap, SCSS, JavaScript, React,
+            Next,  Postman
+          </p>
+          <br />
+          <br />
+          <p className='ppp'>ЯЗЫКИ</p>
+          <br />
+          <br />
+          <p>Английский : 1%</p>
+          <br />
+          <p>Русский : 60%</p>
+          <br />
+
+          <div className="murojat">
+            <a href="https://t.me/Ilkhom_abdumajidov"> Telegram</a>
+          
+            <a href="https://www.instagram.com/ilkhom_abdumajidovv/"> Instagram</a>
+            {/* <p>+998 33 954 40 48  </p> */}
+          </div>
+
+
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
